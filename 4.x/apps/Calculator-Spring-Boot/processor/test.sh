ee65d334180ecab9d833df465736221ed782d056
abalasu1 <abalasu1@in.ibm.com>
2020-08-10 23:25:38 +0530

    eventprocessor

--- 4.x/apps/Calculator-Spring-Boot/processor/test.sh
+++ 4.x/apps/Calculator-Spring-Boot/processor/test.sh
@@ -1,6 +1,6 @@
 while sleep 0.1; 
   do 
-    curl -s -X POST $1/processor/calculate -H 'Content-Type: application/json' -d '{
+    curl -s -X POST $1/processor/mtcalculate -H 'Content-Type: application/json' -d '{
       "x1": "2 %2B 3 %2B 4",
       "x2": "3 - 2",
       "x3": "3 * 2",
