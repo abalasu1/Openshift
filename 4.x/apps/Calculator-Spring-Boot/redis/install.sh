b93e2f158f4f3d5fcc0ea61ec3489d063c57419d
abalasu1 <abalasu1@in.ibm.com>
2020-08-11 08:56:53 +0530

    add redis

---
+++ 4.x/apps/Calculator-Spring-Boot/redis/install.sh
@@ -0,0 +1,2 @@
+oc new-app --docker-image=ibmcom/redis-ppcle64
+oc new-app --docker-image=docker.io/redis
--- 4.x/apps/Calculator-Spring-Boot/zipkin/install.sh
+++ 4.x/apps/Calculator-Spring-Boot/zipkin/install.sh
@@ -1,2 +1,2 @@
-oc new-app --docker-image=ppc64le/zipkin
+oc new-app --docker-image=ibmcom/zipkin-ppcle64
 oc new-app --docker-image=docker.io/zipkin
