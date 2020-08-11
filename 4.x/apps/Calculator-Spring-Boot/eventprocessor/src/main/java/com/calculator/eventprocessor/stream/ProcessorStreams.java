1ef2dbfff91e120eb036df3a85c246118e77d2f7
abalasu1 <abalasu1@in.ibm.com>
2020-08-11 10:03:40 +0530

    remove kafka

--- 4.x/apps/Calculator-Spring-Boot/eventprocessor/src/main/java/com/calculator/eventprocessor/stream/ProcessorStreams.java
+++ 4.x/apps/Calculator-Spring-Boot/eventprocessor/src/main/java/com/calculator/eventprocessor/stream/ProcessorStreams.java
@@ -7,12 +7,12 @@ import org.springframework.messaging.SubscribableChannel;
 
 public interface ProcessorStreams {
 	
-	String INPUT = "calculator-in";
+	/*String INPUT = "calculator-in";
     String OUTPUT = "calculator-out";
     
     @Input(INPUT)
     SubscribableChannel subscribe();
     
     @Output(OUTPUT)
-    MessageChannel notifyTo();
+    MessageChannel notifyTo();*/
 }
